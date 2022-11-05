d56aa75c3cef1740ac18a9ddb0d5c31a34dcd9b9
Florent-Git <florent.ram@gmail.com>
2022-11-05 10:16:15 +0100

    Refactor: Mise en place d'héritage pour les commandes Client/Serveur

--- src/main/java/be/rm/secu/tp1/cli/AESCommandClient.java
+++ src/main/java/be/rm/secu/tp1/cli/AESCommandClient.java
@@ -18,22 +18,7 @@ import java.util.concurrent.Callable;
     description = "Lancement d'un client qui envoie un message encrypté par AES (et D-H) à un serveur"
 )
 
-public class AESCommandClient implements Callable<Integer> {
-    @CommandLine.Option(
-        names = { "-p", "--port" },
-        description = "Port d'écoute du serveur d'encryption (défaut: 56978)"
-    ) private int _port = 56978;
-
-    @CommandLine.Option(
-        names = { "-h", "--host" },
-        description = "Hôte d'écoute du message encrypté"
-    ) private String _host;
-
-    @CommandLine.Option(
-        names = { "-m", "--message" },
-        description = "Le message à encrypter. Ferme le client juste après"
-    ) private String _message;
-
+public class AESCommandClient extends ClientCommand implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         InputStream stdin;
