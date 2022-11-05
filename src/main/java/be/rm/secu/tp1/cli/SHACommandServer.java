a4f27228741e77aa194a6d5cc540257a70d3ed68
rafuryc <rafuryc@hotmail.be>
2022-11-05 10:04:53 +0100

    SHA Server devrait être bon

---
+++ src/main/java/be/rm/secu/tp1/cli/SHACommandServer.java
@@ -0,0 +1,35 @@
+package be.rm.secu.tp1.cli;
+
+import be.rm.secu.tp1.chain.*;
+import be.rm.secu.tp1.net.Server;
+import picocli.CommandLine;
+
+import javax.net.ServerSocketFactory;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
+public class SHACommandServer implements Callable<Integer> {
+    @CommandLine.Option(
+        names = { "-p", "--port" },
+        description = "Port d'écoute (défaut: 56978)"
+    ) private int _port = 56978;
+
+    @Override
+    public Integer call() throws Exception {
+        var executor = Executors.newFixedThreadPool(4);
+
+        var server = Server.builder()
+            .withPort(_port)
+            .withExecutorService(executor)
+            .withServerSocketFactory(ServerSocketFactory.getDefault())
+            .withStdout(System.out)
+            .withInputMiddleware(Middleware.link(
+                new B64DecoderMiddleware(),
+                new SHAServerMiddleware(),
+                new StdoutMiddleware()
+            ))
+            .build();
+
+        return server.call();
+    }
+}
