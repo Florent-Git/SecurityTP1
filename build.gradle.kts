91b369b3a451b15153c96ae302ebea2f261bfa21
Florent-Git <florent.ram@gmail.com>
2022-11-06 16:31:23 +0100

    Ajout d'une configuration fatjar pour que le projet puisse être exécuté à partir d'un fichier jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,28 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
 }
 
+tasks.withType<Jar>() {
+    manifest {
+        attributes("Main-Class" to "be.rm.secu.tp1.Program")
+    }
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    manifest {
+        attributes("Main-Class" to "be.rm.secu.tp1.Program")
+    }
+
+    archiveBaseName.set("${project.name}-all")
+
+    from(configurations.runtimeClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE;
+
+    with(tasks.jar.get() as CopySpec)
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
