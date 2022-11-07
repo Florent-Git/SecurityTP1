9c1b27ea4d3f5ec49a2abc0113adbe3923e06bb8
Florent-Git <florent.ram@gmail.com>
2022-11-07 10:18:50 +0100

    RSA au niveau serveur fonctionnel tant que le client contient le certificat dans son truststore

---
+++ src/main/java/be/rm/secu/tp1/cli/RSACommandServer.java
@@ -0,0 +1,52 @@
+package be.rm.secu.tp1.cli;
+
+import be.rm.secu.tp1.chain.B64DecoderMiddleware;
+import be.rm.secu.tp1.chain.Middleware;
+import be.rm.secu.tp1.chain.RSADecoderMiddleware;
+import be.rm.secu.tp1.chain.StdoutMiddleware;
+import be.rm.secu.tp1.net.Server;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import javax.net.ServerSocketFactory;
+import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
+@Command(
+    name = "rsa-server",
+    description = "Créé un serveur pour écouter un message encrypté par RSA via un certificat dans un keystore java"
+)
+public class RSACommandServer extends ServerCommand implements Callable<Integer> {
+    @Option(
+        names = { "--key-store" },
+        description = "Specifie le certificat à utiliser"
+    ) private File _keyStoreFile;
+
+    @Option(
+        names = { "-P", "--password" },
+        description = "Le mot de passe du keystore"
+    )
+    private String _password;
+
+    @Option(
+        names = { "-a", "--alias" },
+        description = "Alias du certificat dans le keystore"
+    ) private String _alias;
+
+    @Override
+    public Integer call() throws Exception {
+        var server = Server.builder()
+            .withServerSocketFactory(ServerSocketFactory.getDefault())
+            .withPort(_port)
+            .withExecutorService(Executors.newFixedThreadPool(2))
+            .withInputMiddleware(Middleware.link(
+                new B64DecoderMiddleware(),
+                new RSADecoderMiddleware(_keyStoreFile, _alias, _password),
+                new StdoutMiddleware()
+            ))
+            .build();
+
+        return server.call();
+    }
+}
